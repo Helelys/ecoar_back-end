f40aeb084a4f1bbd89ac7e14b869fc6f50754a59
helelys <tallyslabanca20@gmail.com>
2025-04-27 01:32:46 -0300

    Deleting unused token before sent another one

--- src/main/java/com/login/login/service/RequestPasswordService.java
+++ src/main/java/com/login/login/service/RequestPasswordService.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,6 +20,11 @@ public class RequestPasswordService {
     public void resetPasswordUser(EmailDTO email) {
         User user = userRepository.findByEmail(email.getEmail()).orElse(null);
         if (user != null) {
+
+            user.setResetToken(null);
+            user.setDateTime(null);
+            userRepository.save(user);
+
             String token = UUID.randomUUID().toString().substring(0, 6);
 
             user.setResetToken(token);
